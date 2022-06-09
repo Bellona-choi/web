c9835cfdb0418ce13b29598f1b3ce2f34cb9b27a
최재원 <jaewon3588@naver.com>
2022-06-09 13:10:16 +0900

    .

---
+++ j/Word.java
@@ -0,0 +1,26 @@
+
+
+public class Word {
+
+	public static void main(String[] args) {
+		Word word = new Word("Hello World");
+		System.out.println(word.isVowel(0));
+		System.out.println(word.isVowel(1));
+		System.out.println(word.isConsonant(0));
+		System.out.println(word.isConsonant(1));
+	}
+		private String letters;
+		
+		public Word(String letters) {
+			this.letters = letters.toLowerCase();
+		}
+		//aiueo
+		public boolean isVowel(int i) {
+			return "aiueo".contains(letters.substring(i, i + 1));
+		}
+		
+		public boolean isConsonant(int i) {
+			return !"aiueo".contains(letters.substring(i, i + 1));
+		}
+
+	}
